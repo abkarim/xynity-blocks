425e7a20943bf3a3f30cdefb4494425b3d0b4bb5
Karim <akskarim17@gmail.com>
2023-07-24 19:39:25 +0600

    Units added

---
+++ src/components/UnitSelect.jsx
@@ -0,0 +1,18 @@
+import Select from "./Select.jsx";
+import unit from "../util/unit.js";
+
+const UnitSelect = ({ ...props }) => {
+    return (
+        <Select {...props}>
+            {unit().map((item, i) => {
+                return (
+                    <option key={i} value={item.value}>
+                        {item.name}
+                    </option>
+                );
+            })}
+        </Select>
+    );
+};
+
+export default UnitSelect;
---
+++ src/util/unit.js
@@ -0,0 +1,13 @@
+export default function unit() {
+    return [
+        { name: "PX", value: "px" },
+        { name: "REM", value: "rem" },
+        { name: "EM", value: "em" },
+        { name: "%", value: "%" },
+        { name: "VW", value: "vw" },
+        { name: "VH", value: "vh" },
+        { name: "VMIN", value: "vmin" },
+        { name: "VMAX", value: "vmax" },
+        { name: "CH", value: "ch" },
+    ];
+}
