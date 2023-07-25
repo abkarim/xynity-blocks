bc8265906395f04c53eed2b549cfbb9dfc96a18f
Karim <akskarim17@gmail.com>
2023-07-25 19:58:33 +0600

    Nonce constant added

--- xynity-blocks.php
+++ xynity-blocks.php
@@ -47,6 +47,8 @@ if (!function_exists("xynity_blocks_init")) {
         define("XYNITY_BLOCKS_PATH", plugin_dir_path(__FILE__));
         define("XYNITY_BLOCKS_URL", plugin_dir_url(__FILE__));
         define("XYNITY_BLOCKS_BASENAME", plugin_basename(__FILE__));
+        // TODO generate random value
+        define("XYNITY_BLOCKS_NONCE", "settings_update_nonce123");
 
         // Load plugin file
         require_once __DIR__ . "/includes/plugin.php";
