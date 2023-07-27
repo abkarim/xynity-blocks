8c6e44a7708d0ab01c993c89cba6cb96b826d7d9
Karim <akskarim17@gmail.com>
2023-07-27 18:39:54 +0600

    added content properties for notification

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { createRoot } from "react-dom/client";
 
+import { NotificationContextContainer } from "./context/notification.jsx";
+import Notification from "./components/Notification.jsx";
+
 import App from "./App.jsx";
 import "./app.css";
 
 const container = document.getElementById("xynity-blocks-main-container");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+    <NotificationContextContainer>
+        <App />
+        <Notification />
+    </NotificationContextContainer>
+);
