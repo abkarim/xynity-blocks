5a5200ba374d735930276aabde1d6104352a7217
Karim <akskarim17@gmail.com>
2023-07-27 19:58:09 +0600

    returing object instead of group

--- src/util/getUnitAndValue.js
+++ src/util/getUnitAndValue.js
@@ -1,4 +1,7 @@
 export default function getUnitAndValue(string) {
-  const groups = string.match(/(?<value>-?[0-9.]+)(?<unit>[%A-z]+)?/).groups;
-  return groups;
+    const groups = string.match(/(?<value>-?[0-9.]+)(?<unit>[%A-z]+)?/).groups;
+    return {
+        value: groups.value * 1, // multiply by 1 to make it Number from string
+        unit: groups.unit,
+    };
 }
