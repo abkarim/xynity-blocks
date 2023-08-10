d6dadd523f7433fb149b22a219d85be922457737
Karim <akskarim17@gmail.com>
2023-08-10 15:32:07 +0600

    init

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,13 @@
 <?php
+
+// if uninstall.php is not called by WordPress, die
+if (!defined("WP_UNINSTALL_PLUGIN")) {
+    die();
+}
+
+/**
+ * Clear all database properties
+ * and caching configuration
+ *
+ * If user want to uninstall plugin with it's all data
+ */
