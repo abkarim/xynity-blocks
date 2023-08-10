83f807e6ec2cd985c4cfbd890d4015797959e1c5
Karim <akskarim17@gmail.com>
2023-08-10 15:23:26 +0600

    init shadow

---
+++ src/settings/Shadows.jsx
@@ -0,0 +1,14 @@
+const Shadows = () => {
+    return (
+        <section>
+            <div className="flex items-center justify-between px-5 py-2 bg-slate-100">
+                <h2 className="text-sm font-bold text-gray-600 uppercase">
+                    Shadows
+                </h2>
+            </div>
+            <div>Coming soon</div>
+        </section>
+    );
+};
+
+export default Shadows;
