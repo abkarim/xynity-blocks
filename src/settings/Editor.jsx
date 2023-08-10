2367c44386e3eb5ebbbd8a73eca66a5f5c558612
Karim <akskarim17@gmail.com>
2023-08-10 15:18:02 +0600

    updated

--- src/settings/Editor.jsx
+++ src/settings/Editor.jsx
@@ -8,6 +8,7 @@ import Select from "../components/Select.jsx";
 import isArrayEqual from "../util/isArrayEqual.js";
 import RadioSwitchInput from "../components/RadioSwitchInput.jsx";
 import destructFromClamp from "../util/destructFromClamp.js";
+import ChangeIndicator from "./ChangeIndicator.jsx";
 
 /**
  * editor_options_from_backend is localized by WordPress
@@ -76,19 +77,6 @@ const reducer = (state, action) => {
         default:
             return state;
     }
-};
-
-const ChangeIndicator = ({ defaultValue }) => {
-    return (
-        <>
-            <div className="absolute inline-block w-3 h-3 p-1 bg-yellow-300 rounded-full cursor-pointer peer top-2 right-1 aspect-square"></div>
-            <i className="absolute hidden text-3xl text-orange-500 pointer-events-none peer-hover:inline-block -right-1 -top-3 fi fi-rr-caret-down"></i>
-            <p className="absolute hidden p-2 text-white bg-orange-500 rounded-sm pointer-events-none peer-hover:block -right-3 -top-8">
-                (Edited) Default value was&nbsp;
-                <span className="font-bold">{defaultValue}</span>
-            </p>
-        </>
-    );
 };
 
 const Option = ({
