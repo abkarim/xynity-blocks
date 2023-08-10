b002be9e0d56716fc3e074bd7cdea36aa576b01e
Karim <akskarim17@gmail.com>
2023-08-10 14:51:06 +0600

    Utility class added

---
+++ includes/classes/Util.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Xynity_Blocks;
+
+/**
+ * Prevent direct access
+ */
+if (!defined("ABSPATH")) {
+    exit();
+}
+
+class Util
+{
+    /**
+     * Get value if present
+     *
+     * use's isset to check if the value is present or not
+     * if present returns value
+     *
+     * defaults to default value
+     *
+     * @param object
+     * @param mixed - default to return
+     * @since 0.1.1
+     * @access public
+     */
+    public static function get_value_if_present_in_stdClass(
+        $object,
+        $value,
+        $default
+    ) {
+        if (isset($object->$value)) {
+            return $object->$value;
+        }
+
+        return $default;
+    }
+}
