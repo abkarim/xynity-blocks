f99ddd0c6af09ebaafef5cd4e1b9b3b6aec014a6
Karim <akskarim17@gmail.com>
2023-08-12 19:15:36 +0600

    Added smaller props

--- src/components/RadioSwitchInput.jsx
+++ src/components/RadioSwitchInput.jsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 
 const RadioSwitchInput = ({
     selected = false,
-    className,
+    className = "",
+    smaller = false,
     onClick = () => {},
     ...props
 }) => {
     return (
         <div
-            className={`relative z-0 w-10 p-2 rounded-full bg-opacity-60 cursor-pointer ${
+            {...props}
+            className={`relative z-0 ${
+                smaller ? "w-8 p-1.5" : "w-10 p-2"
+            } rounded-full bg-opacity-60 cursor-pointer ${
                 selected ? "bg-green-500" : "bg-gray-400"
             }`}
             onClick={onClick}>
             <span
-                className={`absolute top-0 bottom-0 transition-all inline-block w-4 h-4 rounded-full aspect-square z-[1] scale-150 ${
+                className={`absolute top-0 bottom-0 transition-all inline-block ${
+                    smaller ? "w-3 h-3" : "w-4 h-4"
+                } rounded-full aspect-square z-[1] scale-150 ${
                     selected ? "right-0 bg-green-500" : "left-0 bg-gray-400"
                 }`}></span>
         </div>
