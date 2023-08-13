5068945dfc860b65af55900da7b414b321ad9d74
Karim <akskarim17@gmail.com>
2023-08-13 18:28:01 +0600

    Modifying fonts

--- src/settings/Typography.jsx
+++ src/settings/Typography.jsx
@@ -6,6 +6,10 @@ import ChangeIndicator from "./ChangeIndicator.jsx";
 import getUnitAndValue from "../util/getUnitAndValue.js";
 import Input from "../components/Input.jsx";
 import UnitSelect from "../components/UnitSelect.jsx";
+import TextInput from "../components/InputText.jsx";
+import FontStretchInput from "../components/font/FontStretchInput.jsx";
+import FontStyleInput from "../components/font/FontStyleInput.jsx";
+import FontWeightInput from "../components/font/FontWeightInput.jsx";
 
 /**
  * typography_options_from_backend is localized by WordPress
@@ -38,6 +42,17 @@ const reducer = (state, action) => {
         case "name":
             state[action.category][action.payload.index].name =
                 action.payload.value;
+            return { ...state };
+
+        case "family":
+            state.fontFamilies[action.payload.index].fontFamily =
+                action.payload.data;
+            return { ...state };
+
+        case "face":
+            state.fontFamilies[action.payload.index].fontFace[
+                action.payload.fIndex
+            ][action.payload.name] = action.payload.data;
             return { ...state };
 
         case "slug":
@@ -211,6 +226,7 @@ const Typography = () => {
             textTransform: data.textTransform,
             dropCap: data.dropCap,
             fontSizes: data.fontSizes,
+            fontFamilies: data.fontFamilies,
         };
 
         // Update data
@@ -640,6 +656,240 @@ const Typography = () => {
                                                 dispatch({
                                                     type: "delete",
                                                     category: "fontSizes",
+                                                    payload: {
+                                                        index: i,
+                                                    },
+                                                });
+                                            }}>
+                                            {/* Remove icon */}
+                                            <span className="inline-block w-5 h-5 text-red-600">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    id="Outline"
+                                                    fill="currentColor"
+                                                    viewBox="0 0 24 24">
+                                                    <path d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z" />
+                                                    <path d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z" />
+                                                    <path d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z" />
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+            {/* Font families */}
+            {(visibleOption === "fontFamilies" || visibleOption === "*") && (
+                <div>
+                    <div className="flex items-center justify-between px-5 py-1 border-b bg-gray-50">
+                        <h3 className="text-sm font-bold text-gray-600 uppercase ">
+                            Font Families
+                        </h3>
+                        {/* TODO  */}
+                        {/* <button
+                            className="p-1 px-3 text-white bg-blue-600 rounded-sm"
+                            onClick={() =>
+                                dispatch({
+                                    type: "new",
+                                    category: "fontFamilies",
+                                })
+                            }>
+                            Add New
+                        </button> */}
+                    </div>
+                    {data.fontFamilies.map((font, i) => (
+                        <div
+                            key={i}
+                            className="relative flex items-start justify-between w-full p-5 border-b">
+                            <div>
+                                <div className="space-y-1">
+                                    <input
+                                        type="text"
+                                        className="block w-full text-xl !border-none !outline-none !px-0"
+                                        onInput={(e) => {
+                                            const value = e.target.value;
+                                            dispatch({
+                                                type: "name",
+                                                category: "fontFamilies",
+                                                payload: {
+                                                    index: i,
+                                                    value,
+                                                },
+                                            });
+                                            if (value.trim() === "") {
+                                                errorFound.current = true;
+                                                return setNotification({
+                                                    type: "error",
+                                                    text: "name cannot be empty, please enter a name",
+                                                });
+                                            } else {
+                                                errorFound.current = false;
+                                            }
+                                        }}
+                                        title="name"
+                                        value={font.name}
+                                        placeholder="Type name"
+                                    />
+                                    <input
+                                        title={
+                                            !font.custom
+                                                ? "defaults sizes slug changing is not allowed to prevent style breaking"
+                                                : "slug"
+                                        }
+                                        readOnly={!font.custom}
+                                        value={font.slug}
+                                        className="inline-block !bg-transparent"
+                                        onInput={(e) => {
+                                            const value = e.target.value;
+                                            dispatch({
+                                                type: "slug",
+                                                category: "fontFamilies",
+                                                payload: {
+                                                    index: i,
+                                                    data: value.trim(),
+                                                },
+                                            });
+
+                                            if (value.trim() === "") {
+                                                errorFound.current = true;
+                                                return setNotification({
+                                                    type: "error",
+                                                    text: "slug cannot be empty, please enter a slug",
+                                                });
+                                            } else {
+                                                errorFound.current = false;
+                                            }
+                                        }}
+                                    />
+                                    <br />
+                                    <input
+                                        title={"font family"}
+                                        value={font.fontFamily}
+                                        className="inline-block !bg-transparent"
+                                        onInput={(e) => {
+                                            const value = e.target.value;
+                                            dispatch({
+                                                type: "family",
+                                                payload: {
+                                                    index: i,
+                                                    data: value,
+                                                },
+                                            });
+
+                                            if (value.trim() === "") {
+                                                errorFound.current = true;
+                                                return setNotification({
+                                                    type: "error",
+                                                    text: "family cannot be empty, please enter a slug",
+                                                });
+                                            } else {
+                                                errorFound.current = false;
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex gap-5">
+                                <div className="space-y-2">
+                                    <h2 className="font-bold">Font faces</h2>
+                                    <div className="flex items-center justify-between gap-2">
+                                        <h6>Stretch</h6>
+                                        <h6>Style</h6>
+                                        <h6>Weight</h6>
+                                    </div>
+                                    {font.fontFace?.map((face, index) => (
+                                        <div key={index}>
+                                            <FontStretchInput
+                                                value={face.fontStretch}
+                                                onChange={(e) =>
+                                                    dispatch({
+                                                        type: "face",
+                                                        payload: {
+                                                            index: i,
+                                                            fIndex: index,
+                                                            name: "fontStretch",
+                                                            data: e.target
+                                                                .value,
+                                                        },
+                                                    })
+                                                }
+                                            />
+                                            <FontStyleInput
+                                                value={face.fontStyle}
+                                                onChange={(e) =>
+                                                    dispatch({
+                                                        type: "face",
+                                                        payload: {
+                                                            index: i,
+                                                            fIndex: index,
+                                                            name: "fontStyle",
+                                                            data: e.target
+                                                                .value,
+                                                        },
+                                                    })
+                                                }
+                                            />
+                                            <FontWeightInput
+                                                value={face.fontWeight}
+                                                onChange={(e) =>
+                                                    dispatch({
+                                                        type: "face",
+                                                        payload: {
+                                                            index: i,
+                                                            fIndex: index,
+                                                            name: "fontWeight",
+                                                            data: e.target
+                                                                .value,
+                                                        },
+                                                    })
+                                                }
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <button
+                                        title="Duplicate"
+                                        onClick={() =>
+                                            dispatch({
+                                                type: "duplicate",
+                                                category: "fontFamilies",
+                                                payload: {
+                                                    index: i,
+                                                },
+                                            })
+                                        }>
+                                        {/* Duplicate icon */}
+                                        <span className="inline-block w-5 h-5 ">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                id="Outline"
+                                                viewBox="0 0 24 24"
+                                                fill="currentColor">
+                                                <path d="M21.155,3.272,18.871.913A3.02,3.02,0,0,0,16.715,0H12A5.009,5.009,0,0,0,7.1,4H7A5.006,5.006,0,0,0,2,9V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5v-.1A5.009,5.009,0,0,0,22,14V5.36A2.988,2.988,0,0,0,21.155,3.272ZM13,22H7a3,3,0,0,1-3-3V9A3,3,0,0,1,7,6v8a5.006,5.006,0,0,0,5,5h4A3,3,0,0,1,13,22Zm4-5H12a3,3,0,0,1-3-3V5a3,3,0,0,1,3-3h4V4a2,2,0,0,0,2,2h2v8A3,3,0,0,1,17,17Z" />
+                                            </svg>
+                                        </span>
+                                    </button>
+                                    {font.custom && (
+                                        <button
+                                            title="Delete"
+                                            onClick={() => {
+                                                /**
+                                                 * Get delete confirmation
+                                                 */
+                                                if (
+                                                    !confirm(
+                                                        "are you sure want to delete this font ?"
+                                                    )
+                                                )
+                                                    return;
+
+                                                dispatch({
+                                                    type: "delete",
+                                                    category: "fontFamilies",
                                                     payload: {
                                                         index: i,
                                                     },
