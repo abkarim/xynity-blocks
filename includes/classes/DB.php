edcaa504b0026b51cb573258ac711bccd157d131
Karim <akskarim17@gmail.com>
2023-08-22 19:50:07 +0600

    Creating database

--- includes/classes/DB.php
+++ includes/classes/DB.php
@@ -10,23 +10,187 @@ if (!defined("ABSPATH")) {
 
 class DB
 {
-    private static $options_list = [
+    /**
+     * Current database management version
+     * to update table in database
+     * we use this version
+     */
+    private static $_database_version = "0.1.0";
+
+    /**
+     * Options used in this entire plugin
+     * to store and manage data
+     */
+    private static $_options_list = [
         XYNITY_BLOCKS_TEXT_DOMAIN . "_typography_option",
         XYNITY_BLOCKS_TEXT_DOMAIN . "_shadows_option",
         XYNITY_BLOCKS_TEXT_DOMAIN . "_colors_option",
         XYNITY_BLOCKS_TEXT_DOMAIN . "_settings_option",
+        XYNITY_BLOCKS_TEXT_DOMAIN . "_database_version",
     ];
 
     /**
-     * Remove all options
+     * Checks current database version in this code
+     * with current database version in online database
+     */
+    private static function should_tables_need_to_update()
+    {
+        $current_online_database_version = get_option(
+            XYNITY_BLOCKS_TEXT_DOMAIN . "_database_version",
+            null
+        );
+
+        /**
+         * Table not version not found
+         * this means tables not created
+         */
+        if ($current_online_database_version === null) {
+            return true;
+        }
+
+        /**
+         * Compare local version with online version
+         */
+        if (self::$_database_version !== $current_online_database_version) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Updates or create table if necessary
+     * It should only check on update and activation
      *
-     * @since 0.1.3
+     * @access public
+     * @since 0.1.4
+     */
+    public static function update_tables_if_necessary()
+    {
+        if (!self::should_tables_need_to_update()) {
+            return;
+        }
+
+        /**
+         * Update and create tables
+         */
+        self::create_tables();
+    }
+
+    /**
+     * Get charset collate
+     *
+     * @return string
+     * @access private
+     * @since 0.1.4
+     */
+    public static function get_charset_collate()
+    {
+        global $wpdb;
+        return $wpdb->get_charset_collate();
+    }
+
+    /**
+     * Create tables
+     *
+     * @since 0.1.14
+     * @access private
+     */
+    private static function create_tables()
+    {
+        /**
+         * Include essential file
+         */
+        require_once ABSPATH . "wp-admin/includes/upgrade.php";
+
+        self::create_blocks_settings_table();
+
+        /**
+         * Set database version to current version
+         */
+        update_option(
+            XYNITY_BLOCKS_TEXT_DOMAIN . "_database_version",
+            self::$_database_version
+        );
+    }
+
+    /**
+     * Get blocks settings table name
+     * Returns blocks settings table name with prefix
+     *
+     * @return string
+     * @access public
+     * @since 0.1.4
+     */
+    public static function get_blocks_settings_table_name()
+    {
+        global $wpdb;
+        $table_name = "xynity_blocks__blocks_settings";
+        return $wpdb->prefix . $table_name;
+    }
+
+    /**
+     * Create blocks settings table in database
+     * manage blocks default options
+     * in this table
+     *
+     * @since 0.1.4
+     * @access private
+     */
+    private static function create_blocks_settings_table()
+    {
+        $table_name = self::get_blocks_settings_table_name();
+        $charset_collate = self::get_charset_collate();
+
+        /**
+         * Create table sql
+         */
+        $sql = "CREATE TABLE $table_name (
+                id  mediumint(9) NOT NULL AUTO_INCREMENT,
+                updated_at datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                block_name VARCHAR(500) NOT NULL UNIQUE,
+                block_settings LONGTEXT NULL,
+                PRIMARY KEY (id)
+            ) $charset_collate";
+
+        /**
+         * Execute sql
+         */
+        dbDelta($sql);
+    }
+
+    /**
+     * Delete databases
+     *
+     * @since 0.1.4
+     * @access private
+     */
+    private static function delete_database_tables()
+    {
+        global $wpdb;
+        $table_names = [self::get_blocks_settings_table_name()];
+        $sql = "DROP TABLE IF EXISTS " . join($table_names);
+        $wpdb->query($sql);
+    }
+
+    /**
+     * Clear all data
+     *
+     * @since 0.1.4
      * @access public
      */
-    public static function remove_all_options()
+    public static function clear_all_data()
     {
-        foreach (self::$options_list as $option) {
+        /**
+         * Delete options
+         */
+        foreach (self::$_options_list as $option) {
             delete_option($option);
         }
+
+        /**
+         * Delete databases
+         */
+        self::delete_database_tables();
     }
 }
