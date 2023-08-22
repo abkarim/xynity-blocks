a45f4f8bea055fb31910b804dcead693a04203be
Karim <akskarim17@gmail.com>
2023-08-22 17:32:10 +0600

    migrated to class and handling activation

--- xynity-blocks.php
+++ xynity-blocks.php
@@ -22,42 +22,105 @@ if (!defined("ABSPATH")) {
     exit();
 }
 
-if (!function_exists("xynity_blocks_init")) {
-    /**
-     * Initialize plugin
-     */
-    function xynity_blocks_init()
+if (!class_exists("Xynity_Blocks")) {
+    class Xynity_Blocks
     {
         /**
-         * Get plugin data defined in header
+         * Constructor
          */
-        if (!function_exists("get_plugin_data")) {
-            require_once ABSPATH . "wp-admin/includes/plugin.php";
+        public function __construct()
+        {
+            $this->define_constants();
+
+            /**
+             * Register plugin activation hook
+             */
+            register_activation_hook(XYNITY_BLOCKS_FILE, [
+                $this,
+                "handle_activation",
+            ]);
+
+            /**
+             * Load plugin
+             */
+            add_action("plugins_loaded", [$this, "init"]);
         }
-        $plugin_data = get_plugin_data(__FILE__);
 
         /**
-         * Define essentials constant
+         * Define constant
+         * required in plugin
+         *
+         * @access private
+         * @since 0.1.4
          */
-        define("XYNITY_BLOCKS_REQUIRED_PHP", $plugin_data["RequiresPHP"]);
-        define("XYNITY_BLOCKS_REQUIRED_WP", $plugin_data["RequiresWP"]);
-        define("XYNITY_BLOCKS_VERSION", $plugin_data["Version"]);
-        define("XYNITY_BLOCKS_TEXT_DOMAIN", $plugin_data["TextDomain"]);
-        define("XYNITY_BLOCKS_NAME", $plugin_data["Name"]);
-        define("XYNITY_BLOCKS_PATH", plugin_dir_path(__FILE__));
-        define("XYNITY_BLOCKS_URL", plugin_dir_url(__FILE__));
-        define("XYNITY_BLOCKS_BASENAME", plugin_basename(__FILE__));
-        define(
-            "XYNITY_BLOCKS_NONCE",
-            "2abd9731S07S1b7e9f1DSD2f4E5912e523bc4c80255e3e"
-        );
+        private function define_constants()
+        {
+            /**
+             * Get plugin data defined in header
+             */
+            if (!function_exists("get_plugin_data")) {
+                require_once ABSPATH . "wp-admin/includes/plugin.php";
+            }
+            $plugin_data = get_plugin_data(__FILE__);
 
-        // Load plugin file
-        require_once __DIR__ . "/includes/plugin.php";
+            /**
+             * Define essentials constant
+             */
+            define("XYNITY_BLOCKS_REQUIRED_PHP", $plugin_data["RequiresPHP"]);
+            define("XYNITY_BLOCKS_REQUIRED_WP", $plugin_data["RequiresWP"]);
+            define("XYNITY_BLOCKS_VERSION", $plugin_data["Version"]);
+            define("XYNITY_BLOCKS_TEXT_DOMAIN", $plugin_data["TextDomain"]);
+            define("XYNITY_BLOCKS_NAME", $plugin_data["Name"]);
+            define("XYNITY_BLOCKS_PATH", plugin_dir_path(__FILE__));
+            define("XYNITY_BLOCKS_URL", plugin_dir_url(__FILE__));
+            define("XYNITY_BLOCKS_BASENAME", plugin_basename(__FILE__));
+            define("XYNITY_BLOCKS_FILE", __FILE__);
+            define("XYNITY_BLOCKS_DIR", __DIR__);
+            define(
+                "XYNITY_BLOCKS_NONCE",
+                "2abd9731S07S1b7e9f1DSD2f4E5912e523bc4c80255e3e"
+            );
+        }
 
-        // Run the plugin
-        \Xynity_Blocks\Plugin::instance();
+        /**
+         * Initialize plugin
+         *
+         * Called by plugins_loaded hook
+         *
+         * @access public
+         * @since 0.1.4
+         */
+        public function init()
+        {
+            // Load plugin file
+            require_once XYNITY_BLOCKS_DIR . "/includes/plugin.php";
+
+            // Run the plugin
+            \Xynity_Blocks\Plugin::instance();
+        }
+
+        /**
+         * Activation configure
+         * Performs necessary operation to configure files on activation
+         *
+         * Called by activation hook
+         *
+         * @since 0.1.4
+         * @access public
+         */
+        public function handle_activation()
+        {
+            // Include database class file
+            require_once XYNITY_BLOCKS_DIR . "/includes/classes/DB.php";
+
+            /**
+             * Manage database configuration
+             *
+             * @since 0.1.4
+             */
+            \Xynity_Blocks\DB::update_tables_if_necessary();
+        }
     }
 
-    add_action("plugins_loaded", "xynity_blocks_init");
+    new Xynity_Blocks();
 }
