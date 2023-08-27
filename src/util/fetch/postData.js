fe31ba16a3bb00cd54d8e0b2ac8a71deda27e8fa
karim <akskarim17@gmail.com>
2023-08-27 19:05:53 +0600

    Post data function init

---
+++ src/util/fetch/postData.js
@@ -0,0 +1,22 @@
+/**
+ * Post data to wordpress ajax
+ *
+ * @param {object} param0 - {queryString, action, data}
+ * @returns {Promise}
+ */
+export default function postData({ queryString = "", action = "", data = {} }) {
+    return fetch(
+        `${plugin_info_from_backend.ajax_url}?action=${encodeURIComponent(
+            `xynity_blocks${action}`
+        )}${queryString}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-WP-Nonce": plugin_info_from_backend.ajax_nonce,
+            },
+            credentials: "same-origin",
+            body: JSON.stringify(data),
+        }
+    );
+}
