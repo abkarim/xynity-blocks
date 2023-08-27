4bed6575392cf473e0ec51b8dbe925f34289eb7a
karim <akskarim17@gmail.com>
2023-08-27 19:07:56 +0600

    Editing color functioanlity done

--- includes/classes/Blocks.php
+++ includes/classes/Blocks.php
@@ -11,15 +11,15 @@ if (!defined("ABSPATH")) {
 class Blocks
 {
     /**
-     * Get block data
-     * returns block data
+     * Get block data for editor
+     * returns block data with polyfill data
      *
      * @param string
      * @return object
      * @since 0.1.4
      * @access public
      */
-    public static function get_block_data($block_name)
+    public static function get_block_data_for_editor($block_name)
     {
         $data = DB::get_block_settings($block_name);
 
@@ -35,19 +35,31 @@ class Blocks
             );
             $data = $theme_json_data;
 
-            /**
-             * Styles not exists in theme.json
-             * Not every theme add styles fot every block
-             * Now we had to generate our own data
-             */
             if ($theme_json_data === null) {
-                $data = ThemeActions::generate_block_style_settings();
+                $data = [];
             }
+        } else {
+            $data = json_decode($data, true);
         }
 
-        /**
-         * Check if data contain in database
-         */
+        return $data;
+    }
+
+    /**
+     * Get block data from database
+     * returns block data with
+     *
+     * @param string block_name
+     * @return mixed array||null
+     * @since 0.1.4
+     * @access public
+     */
+    public static function get_block_data_from_database($block_name)
+    {
+        $data = DB::get_block_settings($block_name);
+        if ($data !== null) {
+            $data = json_decode($data, true);
+        }
         return $data;
     }
 
@@ -55,13 +67,106 @@ class Blocks
      * Get edited blocks data
      * returns all blocks data that has been edited
      *
-     * @return array
+     * @return mixed array||null
      * @access public
      * @static
      * @since 0.1.4
      */
     public static function get_edited_blocks_data()
     {
-        return null;
+        $data = DB::get_all_block_settings();
+
+        if (is_null($data)) {
+            return null;
+        }
+        $formattedData = [];
+
+        foreach ($data as $value) {
+            $formattedData[$value["block_name"]] = json_decode(
+                $value["block_settings"],
+                true
+            );
+        }
+
+        return $formattedData;
+    }
+
+    /**
+     * Update block colors
+     *
+     * @param string block_name
+     * @param object colors
+     * @return bool is updated
+     * @static
+     * @access public
+     * @since 0.1.4
+     */
+    public static function update_block_colors($block_name, $colorsObject)
+    {
+        $colors = [];
+        /**
+         * Color has 3 elements
+         * [text, background, link]
+         *
+         * we have to include only valid (non empty) data in settings
+         */
+        $text = Util::get_value_if_present_in_stdClass(
+            $colorsObject,
+            "text",
+            null
+        );
+        $background = Util::get_value_if_present_in_stdClass(
+            $colorsObject,
+            "background",
+            null
+        );
+        $link = Util::get_value_if_present_in_stdClass(
+            $colorsObject,
+            "link",
+            null
+        );
+
+        /**
+         * Include valid colors
+         */
+        if ($text !== null && trim($text) !== "") {
+            $colors["text"] = $text;
+        }
+        if ($background !== null && trim($background) !== "") {
+            $colors["background"] = $background;
+        }
+        if ($link !== null && trim($link) !== "") {
+            $colors["link"] = $link;
+        }
+
+        /**
+         * No valid colors found
+         */
+        if (count($colors) === 0) {
+            return true;
+        }
+
+        /**
+         * Get previous data from database
+         */
+        $previous_settings_array = self::get_block_data_from_database(
+            $block_name
+        );
+        $result = false;
+
+        if ($previous_settings_array === null) {
+            $data = [
+                "color" => $colors,
+            ];
+            $result = DB::insert_block_settings($block_name, $data);
+        } else {
+            $previous_settings_array["color"] = $colors;
+            $result = DB::update_block_settings(
+                $block_name,
+                $previous_settings_array
+            );
+        }
+
+        return filter_var($result, FILTER_VALIDATE_BOOLEAN);
     }
 }
