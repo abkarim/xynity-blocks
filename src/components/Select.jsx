0fa80a6406ed4511a0a6e0c186e2659ed8c3274b
karim <akskarim17@gmail.com>
2023-09-05 11:08:43 +0600

    changed height to auto

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,7 +2,7 @@ export default function Select({ className = "", children, ...props }) {
     return (
         <select
             {...props}
-            className={`p-2 border-2 h-full bg-gray-50 cursor-pointer rounded-sm outline-none focus:outline-none border-red-700 ${className}`}>
+            className={`p-2 border-2 h-auto bg-gray-50 cursor-pointer rounded-sm outline-none focus:outline-none border-red-700 ${className}`}>
             {children}
         </select>
     );
