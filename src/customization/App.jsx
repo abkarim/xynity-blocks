80198bb2064ae7ac1a147ceed6d8339b0ef396e4
Karim <akskarim17@gmail.com>
2023-10-21 16:34:54 +0600

    default

--- src/customization/App.jsx
+++ src/customization/App.jsx
@@ -2,8 +2,6 @@ import { Suspense, lazy, useState } from "react";
 import getSearchParam from "../util/getSearchParam";
 import appendSearchParam from "../util/appendSearchParam";
 const Icon = lazy(() => import("./Icon.jsx"));
-const CSS = lazy(() => import("./CSS.jsx"));
-const JavaScript = lazy(() => import("./JavaScript.jsx"));
 
 const SideBar = ({ currentOption, setOption }) => {
     const Option = ({ targetOption, children }) => {
@@ -34,33 +32,6 @@ const SideBar = ({ currentOption, setOption }) => {
                 </span>
                 Icon
             </Option>
-            <Option targetOption="css">
-                <span className="inline-block w-5 h-full">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        version="1.1"
-                        id="Capa_1"
-                        x="0px"
-                        y="0px"
-                        fill="currentColor"
-                        viewBox="0 0 24 24">
-                        <polygon points="6.972,14.935 7.274,18.316 11.994,19.59 11.998,19.589 11.998,19.589 16.725,18.313 17.217,12.816 2.528,12.816   2.149,8.578 17.584,8.578 17.97,4.238 1.771,4.238 1.385,0 22.615,0 20.686,21.59 12.013,23.994 12.013,23.995 11.993,24   3.312,21.59 2.718,14.935 " />
-                    </svg>
-                </span>
-                CSS
-            </Option>
-            <Option targetOption="js">
-                <span className="inline-block w-5 h-full mt-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        id="js"
-                        fill="currentColor"
-                        viewBox="6.3 10.87 15.85 11.3">
-                        <path d="M16.122,18.75a2.456,2.456,0,0,0,2.225,1.37c.934,0,1.531-.467,1.531-1.113,0-.773-.613-1.047-1.642-1.5l-.564-.242c-1.627-.693-2.708-1.562-2.708-3.4a3.014,3.014,0,0,1,3.3-2.979A3.332,3.332,0,0,1,21.474,12.7l-1.756,1.127a1.534,1.534,0,0,0-1.451-.966.982.982,0,0,0-1.08.966c0,.677.419.951,1.387,1.37l.564.241c1.916.822,3,1.66,3,3.543,0,2.031-1.595,3.143-3.737,3.143a4.333,4.333,0,0,1-4.11-2.306Zm-7.967.2c.354.628.677,1.16,1.451,1.16.741,0,1.209-.29,1.209-1.418V11.02H13.07v7.7a3.063,3.063,0,0,1-3.368,3.4,3.5,3.5,0,0,1-3.383-2.06Z"></path>{" "}
-                    </svg>
-                </span>
-                JavaScript
-            </Option>
         </aside>
     );
 };
@@ -83,8 +54,6 @@ const Customization = () => {
                 <figure className="w-full">
                     <Suspense fallback={<h1>Loading...</h1>}>
                         {option === "icon" && <Icon />}
-                        {option === "css" && <CSS />}
-                        {option === "js" && <JavaScript />}
                     </Suspense>
                 </figure>
             </div>
