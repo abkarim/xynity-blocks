605bd13f7bcc7cc766563003261d655243e7da1a
Karim <akskarim17@gmail.com>
2023-12-19 18:18:36 +0600

    Making slider interactive on frontend

--- blocks/src/slider/view.js
+++ blocks/src/slider/view.js
@@ -1,25 +1,36 @@
-/**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
- */
+import Control from "./Control";
 
-/* eslint-disable no-console */
-console.log("Hello World! (from create-block-slider block)");
-/* eslint-enable no-console */
+/**
+ * Get all sliders
+ */
+const sliders = [
+	...document.querySelectorAll(".wp-block-xynity-blocks-slider"),
+];
+
+sliders.map((sliderContainer) => {
+	const contentElement = sliderContainer.querySelector(".content");
+	const control = new Control(contentElement);
+
+	/**
+	 * Add events to controller if exists
+	 */
+	const controllerContainerElement =
+		sliderContainer.querySelector(".controller");
+	if (controllerContainerElement) {
+		const previousControllerElement =
+			controllerContainerElement.querySelector(".previous");
+		previousControllerElement.addEventListener("click", () =>
+			control.previousSlide()
+		);
+
+		const nextControllerElement =
+			controllerContainerElement.querySelector(".next");
+		nextControllerElement.addEventListener("click", () =>
+			control.nextSlide()
+		);
+	}
+
+	/**
+	 * Add events to indicator if exists
+	 */
+});
