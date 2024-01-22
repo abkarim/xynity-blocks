e8e6b998de86617a98386e705658205271c2dbac
Karim <akskarim17@gmail.com>
2024-01-23 03:46:48 +0600

    slider remove button added

--- blocks/src/slider-child/edit.js
+++ blocks/src/slider-child/edit.js
@@ -1,15 +1,45 @@
 import { __ } from "@wordpress/i18n";
-import { InnerBlocks, useBlockProps } from "@wordpress/block-editor";
+import {
+	BlockControls,
+	InnerBlocks,
+	useBlockProps,
+} from "@wordpress/block-editor";
+import { Toolbar, ToolbarButton, Icon } from "@wordpress/components";
+import { trash } from "@wordpress/icons";
+
 import "./editor.scss";
+import { useDispatch } from "@wordpress/data";
 
 /**
  * Allowed blocks in innerBlocks
  */
 const ALLOWED_BLOCKS = ["core/image"];
 
-export default function Edit() {
+export default function Edit({ clientId }) {
+	const { removeBlock } = useDispatch("core/block-editor");
+
+	/**
+	 * Removes current slide
+	 */
+	function removeSlide() {
+		/**
+		 * ? History doesn't work properly when removed with it
+		 */
+		removeBlock(clientId);
+	}
+
 	return (
 		<section {...useBlockProps()}>
+			<BlockControls>
+				<Toolbar label="Slider Controls">
+					<ToolbarButton
+						showTooltip={true}
+						label="Delete current slide"
+						onClick={removeSlide}>
+						<Icon icon={trash} />
+					</ToolbarButton>
+				</Toolbar>
+			</BlockControls>
 			<InnerBlocks
 				allowedBlocks={ALLOWED_BLOCKS}
 				template={[["core/image", {}]]}
