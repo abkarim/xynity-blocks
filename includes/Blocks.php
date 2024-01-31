48a77e588f18113142f08ecd9306e6577fbe5b91
Karim <akskarim17@gmail.com>
2024-02-01 01:19:03 +0600

    methods added for activate and deactivate

--- includes/Blocks.php
+++ includes/Blocks.php
@@ -11,11 +11,20 @@ if (!defined("ABSPATH")) {
 
 class Blocks
 {
+    public static $_activated_blocks_option_name = XYNITY_BLOCKS_TEXT_DOMAIN . "_activated_blocks";
+    public static $_deactivated_blocks_option_name = XYNITY_BLOCKS_TEXT_DOMAIN . "_deactivated_blocks";
+
+    /**
+     * Blocks list to be activated by default
+     * @var array
+     */
+    public static $_blocks_to_register_by_default = ["slider", "slider-child"];
+
     /**
      * Blocks to register 
      * @var array
      */
-    protected $_blocks_to_register = ["slider", "slider-child"];
+    protected $_blocks_to_register = [];
 
     /**
      * Constructor
@@ -25,31 +34,74 @@ class Blocks
      */
     public function __construct()
     {
+        $this->get_activate_blocks_from_database();
         $this->register_blocks();
 
         add_action('enqueue_block_assets', [$this, 'enqueue_block_assets']);
     }
 
     /**
-     * Remove disabled block from $_blocks_to_register 
-     * this will prevent disabled blocks from being register 
+     * Get activated blocks from database
+     * and update $this->_blocks_to_register
      * 
+     * @since 0.2.7
+     * @access protected
+     */
+    protected function get_activate_blocks_from_database()
+    {
+        $activated_blocks = get_option(self::$_activated_blocks_option_name, serialize([]));
+        $this->_blocks_to_register = unserialize($activated_blocks);
+    }
+
+    /**
+     * Activate block
+     * 
+     * @param string block-name
      * @return void
      * @access protected
      * @since 0.2.7
      */
-    protected function remove_disabled_block_names(): void
+    protected function activate_block(string $block_name): void
     {
         /**
-         * Get Disabled blocks from Database
-         * @var array
+         * Remove from deactivated_blocks list option
          */
-        $disabled_blocks = [];
+        $deactivated_blocks = get_option(self::$_deactivated_blocks_option_name, serialize([]));
+        unset($block_name);
+        update_option(self::$_deactivated_blocks_option_name, serialize($deactivated_blocks));
 
         /**
-         * Remove disabled blocks
+         * Add to activated_blocks list option
          */
-        $this->_blocks_to_register = array_diff($this->_blocks_to_register, $disabled_blocks);
+        $activated_blocks = get_option(self::$_activated_blocks_option_name, serialize([]));
+        $activated_blocks[] = $block_name;
+        update_option(self::$_activated_blocks_option_name, serialize($activated_blocks));
+    }
+
+
+    /**
+     * Deactivate block
+     * 
+     * @param string block-name
+     * @return void
+     * @access protected
+     * @since 0.2.7
+     */
+    protected function deactivate_block(string $block_name): void
+    {
+        /**
+         * Remove from activated_blocks list option
+         */
+        $activated_blocks = get_option(self::$_activated_blocks_option_name, serialize([]));
+        unset($block_name);
+        update_option(self::$_activated_blocks_option_name, serialize($activated_blocks));
+
+        /**
+         * Add to deactivated_blocks list option
+         */
+        $deactivated_blocks = get_option(self::$_deactivated_blocks_option_name, serialize([]));
+        $deactivated_blocks[] = $block_name;
+        update_option(self::$_deactivated_blocks_option_name, serialize($deactivated_blocks));
     }
 
     /**
